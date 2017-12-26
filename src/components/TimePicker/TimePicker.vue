ca548e191189e21f25bf7daed62fa89a4dbb371c
Unknown <kalt.steven@gmail.com>
2017-12-25 21:45:43 -0500

    Imported timepicker as a package

--- src/components/TimePicker/TimePicker.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <span>
-    <input type="date" @input="setDate"/>
-    <input type="time" @input="setTime"/>
-  </span>
-</template>
-<script src="./logic.js"></script>
-
-<style src="./style.css"></style>
